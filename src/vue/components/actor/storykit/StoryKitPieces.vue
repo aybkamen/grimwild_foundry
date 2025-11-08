aeeb8b7f54dcc90cbf69ddfd5b9b7ddeaa349712
Aybkamen <aybkamen2@gmail.com>
2025-11-08 13:03:38 +0100

    story kits mix it up

--- src/vue/components/actor/storykit/StoryKitPieces.vue
+++ src/vue/components/actor/storykit/StoryKitPieces.vue
@@ -4,7 +4,7 @@
       <div class="item-name">{{ game.i18n.localize('GRIMWILD.Actor.StoryKit.Tabs.Pieces') }}</div>
       <div class="item-controls" v-if="context.editable">
         <button class="item-control item-create" title="Add piece"
-                data-action="createArrayEntry" data-field="system.pieces">
+                data-action="createArrayEntry" data-field="pieces">
           <i class="fas fa-plus"></i><span>Add</span>
         </button>
       </div>
@@ -13,7 +13,7 @@
       <li v-for="(piece, key) in context.system.pieces" :key="key" class="item flexcol">
         <div class="item-name">
           <input type="text" :name="`system.pieces.${key}.title`" v-model="piece.title" placeholder="Title"/>
-          <a class="item-control item-delete" title="Delete piece" data-action="deleteArrayEntry" data-field="system.pieces" :data-key="key"><i class="fas fa-trash"></i></a>
+          <a class="item-control item-delete" title="Delete piece" data-action="deleteArrayEntry" data-field="pieces" :data-key="key"><i class="fas fa-trash"></i></a>
         </div>
         <div class="item-description">
           <Prosemirror :editable="context.editable" :field="context.editors[`system.pieces.${key}.description`]"/>
